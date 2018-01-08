d65f5f29034f7538c30b10b8df055407eceebc7e
worbit <bernhard@arch.ethz.ch>
2018-01-08 16:27:38 +0100

    update Constants

--- 00_computational_design/sketch_Test/Constants.py
+++ 00_computational_design/sketch_Test/Constants.py
@@ -1,8 +1,8 @@
-typeStreet = 0
+typeStreet = 0      # team traffic creates streets and blocks
 typeBlock = 1
-typePlotEmpty = 2
+typePlotEmpty = 2   # team urban planner input: blocks output: PlotEmpty,Highrise and Lowries
 typePlotHighrise = 3
 typePlotLowrise = 4
-typeFacade1 = 5
-typeFacade2 = 6
-typeRoof = 7
+typeFacade1 = 5     # teams building input plots output: facades and roofs
+typeFacade2 = 6     # team facade: input typeFacade1,typeFacade2,  output: windows, baywindows or balcony
+typeRoof = 7        # team roof: input roof output different kind of roof geometry
